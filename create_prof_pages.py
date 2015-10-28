742e9f91243d400e2c7adf8d664d43472e098a1a
Harsh Gupta <gupta.harsh96@gmail.com>
2015-10-28 15:30:11 +0530

    Update page to have new information
    Signed-off-by: Harsh Gupta <gupta.harsh96@gmail.com>

--- create_prof_pages.py
+++ create_prof_pages.py
@@ -17,15 +17,26 @@ for prof in prof_details:
     prof["field"] = "| ".join(prof["field"])
     prof_text = """
 {{Infobox Professor
+| image = [[File:%(name)s.jpg]]
 | department = %(dept)s
 | research_areas = {{ubl| %(field)s}}
 | year_joined = %(year)s
+| email= %(email)s
+| website= %(website)s
 }}
+
+== See Also ==
+
+* [[Department of %(dept)s]]
+
+== External links ==
+
+* [%(link)s %(name)s] on iitkgp.ac.in
 """
     prof_text = prof_text % prof
     prof_page = pywikibot.Page(site, prof['name'])
     prof_page.text = prof_text
 
-    msg = "Creat Page for {}".format(prof['name'])
+    msg = "Created Page for {}".format(prof['name'])
     prof_page.save(msg)
     print(msg)
